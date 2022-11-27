8ff7698197347dffd37fd2f2724cdff51cbebb63
jnosowska <joanna.nosowska.91@gmail.com>
2022-11-27 21:48:07 +0100

    finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+for (ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  list.append(item);
+}
