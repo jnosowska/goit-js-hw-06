3bacf50138b67a5b3ca96fae4bb3ccab6b398330
jnosowska <joanna.nosowska.91@gmail.com>
2023-01-10 21:35:26 +0100

    update 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector(".change-color");
+const colorHolder = document.querySelector(".color");
+
+colorBtn.addEventListener("click", () => {
+  let generatedColor = getRandomHexColor();
+  colorHolder.innerHTML = generatedColor;
+  document.body.style.backgroundColor = generatedColor;
+});
